236e3665dfd4cc021cc2a76482eba54218c0a507
Viktor Leite <vitorlesantos@outlook.com>
2024-01-19 14:20:30 -0400

    treatment Related Manager created

--- app/Filament/Resources/PatientResource/RelationManagers/TreatmentsRelationManager.php
+++ app/Filament/Resources/PatientResource/RelationManagers/TreatmentsRelationManager.php
@@ -19,8 +19,16 @@ class TreatmentsRelationManager extends RelationManager
         return $form
             ->schema([
                 Forms\Components\TextInput::make('description')
-                    ->required()
-                    ->maxLength(255),
+                ->required()
+                ->maxLength(255)
+                ->columnSpan('full'),
+            Forms\Components\Textarea::make('notes')
+                ->maxLength(65535)
+                ->columnSpan('full'),
+             Forms\Components\TextInput::make('price')
+                ->numeric()
+                ->prefix('€')
+                ->maxValue(42949672.95),
             ]);
     }
 
